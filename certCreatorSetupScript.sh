81e2b4024f4ede46423b3f287572f84c40e92d07
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-28 12:57:14 -0400

    Update certCreatorSetupScript.sh

--- certCreatorSetupScript.sh
+++ certCreatorSetupScript.sh
@@ -7,8 +7,38 @@ echo "This script will walk you through the creation of user certificates, inclu
 read -p "Press any key to being setup..."
 
 cd /opt/tak/certs/
-
 mkdir /opt/tak/certs/files/clients
+
+
+#Figure out which truststore to use
+while true; do
+echo "Does your server use Certificate Enrollment (intermediate CA)?"
+echo "Y: Yes (use truststore-intermediate-CA.p12)"
+echo "N: No (use truststore-root.p12)"
+echo "(if unsure, choose N)"
+read transferoption
+        case $transferoption in
+                [Yy]* )
+                        TRUSTSTORE="truststore-intermediate-CA.p12"
+                        break
+                ;;
+                [Nn]* )
+                        TRUSTSTORE="truststore-root.p12"
+                        break
+                ;;
+                * ) echo "You must select a valid option to continue";;
+        esac
+done
+
+#What port does server use?
+echo "What port does your TAK server use for COT Streaming?"
+echo "(Leave blank and hit enter to use default 8089)"
+read TAK_COT_PORT
+if [ -z "${TAK_COT_PORT}" ]; then 
+    TAK_COT_PORT='8089'
+else 
+    TAK_COT_PORT=${TAK_COT_PORT}
+fi
 
 #Make the Client Keys
 echo "How many clients do you want to configure?"
@@ -28,9 +58,60 @@ do
     echo "Creating certs for $CLIENT_NAME"
     ./makeCert.sh client tc-$CLIENT_NAME
     
-    cp ./files/tc-$CLIENT_NAME.p12 ./files/clients/
+    #Make a folder per user
+    mkdir /opt/tak/certs/files/clients/$CLIENT_NAME
 
- done 
+    #Copy over client certs
+    cp /opt/tak/certs/files/tc-$CLIENT_NAME.p12 /opt/tak/certs/files/clients/$CLIENT_NAME
+    #Iphone files setup
+    cp /opt/tak/certs/files/clients/$CLIENT_NAME/tc-$CLIENT_NAME.p12 /opt/tak/certs/files/clients/$CLIENT_NAME/iphone.p12
+    cp /opt/tak/certs/files/$TRUSTSTORE /opt/tak/certs/files/clients/$CLIENT_NAME
+    mv /opt/tak/certs/files/clients/$CLIENT_NAME/$TRUSTSTORE /opt/tak/certs/files/clients/$CLIENT_NAME/server.p12
+
+sudo tee /opt/tak/certs/files/clients/$CLIENT_NAME/manifest.xml >/dev/null << EOF
+    <MissionPackageManifest version="2">
+    <Configuration>
+    <Parameter name="uid" value="bcfaa4a5-2224-4095-bbe3-fdaa22a82741"/>
+    <Parameter name="name" value="testbox_DP"/>
+    <Parameter name="onReceiveDelete" value="true"/>
+    </Configuration>
+    <Contents>
+    <Content ignore="false" zipEntry="certs\taky-server.pref"/>
+    <Content ignore="false" zipEntry="certs\server.p12"/>
+    <Content ignore="false" zipEntry="certs\iphone.p12"/>
+    </Contents>
+    </MissionPackageManifest>
+EOF
+
+DEVICE_NAME=$(ip -o -4 route show to default | awk '{print $5}')
+#echo "FOUND DEVICE NAME: $DEVICE_NAME"
+
+PUB_SERVER_IP=$(ip addr show $DEVICE_NAME | awk 'NR==3{print substr($2,1,(length($2)-3))}')
+echo "SERVER IP FOR ITAK CONFIG USED: $PUB_SERVER_IP"
+
+sudo tee /opt/tak/certs/files/clients/$CLIENT_NAME/taky-server.pref >/dev/null << EOF
+<?xml version='1.0' encoding='ASCII' standalone='yes'?>
+<preferences>
+  <preference version="1" name="cot_streams">
+    <entry key="count" class="class java.lang.Integer">1</entry>
+    <entry key="description0" class="class java.lang.String">ATAKHQ</entry>
+    <entry key="enabled0" class="class java.lang.Boolean">true</entry>
+    <entry key="connectString0" class="class java.lang.String">$PUB_SERVER_IP:$TAK_COT_PORT:ssl</entry>
+  </preference>
+  <preference version="1" name="com.atakmap.app_preferences">
+    <entry key="displayServerConnectionWidget" class="class java.lang.Boolean">true</entry>
+    <entry key="caLocation" class="class java.lang.String">cert/server.p12</entry>
+    <entry key="caPassword" class="class java.lang.String">atakatak</entry>
+    <entry key="clientPassword" class="class java.lang.String">atakatak</entry>
+    <entry key="certificateLocation" class="class java.lang.String">cert/iphone.p12</entry>
+  </preference>
+</preferences>
+EOF
+
+cd /opt/tak/certs/files/clients/$CLIENT_NAME/
+zip itak.zip iphone.p12 server.p12 manifest.xml taky-server.pref
+
+done 
  
  echo "******************************************************************************"
  echo "******************************************************************************"
