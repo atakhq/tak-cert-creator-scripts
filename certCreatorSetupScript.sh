f02d1bfb02b789f95de3986038c88355fe9c82c8
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-28 11:15:15 -0400

    Create certCreatorSetupScript.sh

---
+++ certCreatorSetupScript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "YOU MUST RUN THIS FROM INSIDE YOUR TAK DOCKER CONTAINER!!!!"
+read -p "Press any key to being setup..."
+
+cd opt/tak/certs/
+
+#Make the Client Keys
+echo "How many clients do you want to configure?"
+read CLIENT_COUNT
+CLIENT_ARR=()
+for ((i=1; i<=$CLIENT_COUNT;i++))
+
+do
+    CLIENT_ARR+=($CLIENT_NAME)
+    echo "What is the username for client #$i?"
+    read CLIENT_NAME
+    
+    echo "Creating certs for $CLIENT_NAME"
+    ./makeCert.sh client $CLIENT_NAME
+
+ done 
+ 
+ echo "******************************************************************************"
+ echo "******************************************************************************"
+ echo "All certs have been created, please exit docker shell and run the final script"
+ echo ""
+ echo "exit"
+ echo ""
+ echo "cd /tmp/tak-cert-enrollment-script/ && chmod +x * && . certEnrollPostInstallScript.sh"
